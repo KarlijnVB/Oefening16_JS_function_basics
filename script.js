90b74e4b2aac6287b38c2d76da4b2456219add8d
KarlijnVB <90220989+KarlijnVB@users.noreply.github.com>
2021-11-14 16:46:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const makePizza = function () {
+    console.log('Make dough')
+    console.log('Make tomato sauce')
+    console.log('Cut vegetables')
+    console.log('Combine')
+    console.log('Bake in oven')
+};
+
+const makeSushi = function () {
+    console.log('Boil rice')
+    console.log('Cut vegetables')
+    console.log('Roll the sushi')
+    console.log('Cut the sushi')
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
+
